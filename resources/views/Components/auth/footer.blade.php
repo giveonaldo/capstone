e25c50dd4cff71370341d34968bf78039c194bc3
giveonaldo <giveonaldo@gmail.com>
2025-01-06 12:42:12 +0700

    done with register user

---
+++ resources/views/Components/auth/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="grid grid-cols-3 justify-items-center py-8 border-t-2">
+    <h3>&copy; 2024 All Rights Reserved</h3>
+    <a class="hover:underline" href="/regulations">Privacy Policy</a>
+    <a class="hover:underline" href="/termService">Term of Services</a>
+</footer>
