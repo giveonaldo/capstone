c7bbd3863b9f536cecc5763a458046065cd3d806
giveonaldo <giveonaldo@gmail.com>
2025-01-06 13:26:37 +0700

    done with login and authentication

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\PetaniController;
 use App\Http\Controllers\RegisterUserController;
-use App\Models\Petani;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 // Home Route
 Route::view('/', 'home');
 
 // Register and Login Users
-Route::get('/register', [RegisterUserController::class, 'create']);
-Route::post('/register', [RegisterUserController::class, 'store']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisterUserController::class, 'create']);
+    Route::post('/register', [RegisterUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+});
